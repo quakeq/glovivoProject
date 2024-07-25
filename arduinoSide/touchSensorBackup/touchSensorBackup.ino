7c8f5800e2b1e7f136535fbe0d571cef102e3fcb
quakeq <thomasahn2009@gmail.com>
2024-07-25 14:40:38 -0700

    Test for Pressure Sensors
    As title says. Should work for flex sensors

--- arduinoSide/touchSensorBackup/touchSensorBackup.ino
+++ arduinoSide/touchSensorBackup/touchSensorBackup.ino
@@ -1,5 +1,5 @@
-int touched[5]; //bool array checks if each sensor is activated
-const int numFing = 5; //easy to change number of fingers
+int touched[2]; //bool array checks if each sensor is activated
+const int numFing = 2; //easy to change number of fingers
 
 void setup() {
   //Average set serial to 9600
@@ -8,17 +8,17 @@ void setup() {
 
 void loop() {
   //For loop checks if fingers are activated
-  for (j = 0; j < numFing; j++){
+  for (int j = 0; j < numFing; j++){
     touched[j] = analogRead(j);
   }
   //Writes out the flexAmp amount
-  for (i = 0; i < numFing; i++){
+  for (int i = 0; i < numFing; i++){
     if (touched[i] >= 10){
-      Serial.println("Finger" + i + ":" + touched[i]);
+      Serial.println(touched[i]);
     }
     else {
       //If small bend, then it doesn't trigger a bang, meaning finger's sound doesn't play
-      Serial.println("Finger" + i + ":" + "OFF"); 
+      Serial.println("Finger: OFF"); 
     }
   }
   delay(100); //small delay
