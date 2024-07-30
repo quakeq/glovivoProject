1aac4f93ca81eafb4ee622497ca3ef044296f028
quakeq <thomasahn2009@gmail.com>
2024-07-30 15:21:31 -0700

    YIPPIE WORKY WORKY FSR

--- arduinoSide/mainHandReciverCode/testESP32s/testESP32s.ino
+++ arduinoSide/mainHandReciverCode/testESP32s/testESP32s.ino
@@ -1,26 +1,31 @@
 #include "BluetoothSerial.h"
 
-#define FSR_0 12
-#define FSR_1 14
-#define FSR_2 
-#define FSR_3 
-#define FSR_4
-BluetoothSerial SerialBT;
-int numFinger = 2;
-int touch[2];
+#define FSR_0 36
+#define FSR_1 39
+#define FSR_2 34
 
+#define FLEX_0 32
+#define FLEX_1 33
+
+BluetoothSerial SerialBT;
+
+int numFinger = 3;
+int numFlex = 2;
+int touch[3];
+int flex[2];
 
 void setup() {
   // put your setup code here, to run once:
   SerialBT.begin("esp32r");
   Serial.begin(115200);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   touch[0] = analogRead(FSR_0);
   touch[1] = analogRead(FSR_1);
-
+  touch[2] = analogRead(FSR_2);
   for (int i = 0; i < numFinger; i++){
     if (touch[i] >= 10){
       SerialBT.print(touch[i]); SerialBT.print(" "); SerialBT.print("1"); SerialBT.print(" ");
@@ -29,7 +34,8 @@ void loop() {
       SerialBT.print("0"); SerialBT.print(" "); SerialBT.print("0"); SerialBT.print(" "); 
     }
   }
+  
   SerialBT.println(" ");
 
-  delay(10);
+  delay(50);
 }
